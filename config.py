209607c1e7574212682750c6089c84f00757261d
Yosuke775 <priyanshuprasad775@gmail.com>
2025-06-30 23:41:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 API_ID = "23476863" #API ID get it from telegram.org
 API_HASH = "69daa0835439c4211f34c2e9ad0acb5c" #API HASH get it from telegram.org
-BOT_TOKEN = "" #Bot Token get it from @BotFather
+BOT_TOKEN = "7921810145:AAGEm9Jq869GGKQXNK_FcRx1g6DrJZzjStY" #Bot Token get it from @BotFather
 URL_A = "https://myanimelist.net/rss/news.xml" #News RSS Feed Url 2
 URL_B = "https://feeds.feedburner.com/crunchyroll/rss" #News RSS Feed Url 2
 START_PIC = "https://images6.alphacoders.com/131/1313754.png" #Startup Pic [Optional]
